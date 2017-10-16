fe76f0a346c2b698aa67a2aac78fe0e965b77762
ChikahoTakayama <puddingstorms@gmail.com>
2017-10-16 14:55:48 +0900

    update pet.cpp #4

--- pet.cpp
+++ pet.cpp
@@ -9,6 +9,7 @@ class Pet // クラス Pet を定義する
 {
 public: // 以下のメンバー変数やメソッドを公開する
     virtual string GetAngry() = 0; // 抽象メソッド
+    virtual string rejoice() = 0; 
     virtual ~Pet() {} // デストラクタ: delete で呼ばれる
 };
 
@@ -18,6 +19,10 @@ public:
     virtual string GetAngry() // C++ ではメソッドに virtual とつけるとポリモーフィズムが利くようになる
     {
         return "ネコがひっかく、毛を逆立てる";
+    }
+  virtual string rejoice() // C++ ではメソッドに virtual とつけるとポリモーフィズムが利くようになる
+    {
+        return "ネコが喉を鳴らす";
     }
 };
 
@@ -29,16 +34,22 @@ public:
     {
         return "イヌがほえる、噛む";
     }
+      virtual string rejoice()
+    {
+        return "イヌが尻尾を振る";
+    }
 };
 
 int main()
 {
     Pet* pet = new Cat(); // Cat クラスのインスタンス(実体)を新規作成し，ポインタ変数 pet に代入する
     cout << pet->GetAngry() << endl; // C言語風に書くと printf("%s\n", pet->GetAngry());
+    cout << pet->rejoice() << endl;
     delete pet; // C++ ではオブジェクトを使い終わったら必ず delete する
 
     pet = new Dog();
     cout << pet->GetAngry() << endl; // C言語風に書くと printf("%s\n", pet->GetAngry());
+     cout << pet->rejoice() << endl;
     delete pet;
 
     return 0;
